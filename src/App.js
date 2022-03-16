709683e0b97b8581dde3beb05babe8f870a5bfea
TXT-34 <txt-34@TXT-34.lan>
2022-03-15 20:48:26 -0700

    I added the components and working on adding all the assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Route path="/" exact component={Home} />
+      <Route path="/AboutUs" exact component={AboutUs} />
+      <Route path="/Roadmap" exact component={Roadmap} />
+      <Route path="/Team" exact component={Team} />
+      <Route path="/Footer" exact component={Footer} /> */}
     </div>
   );
 }
-
+// links
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function NavBar() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+
+export default NavBar;
